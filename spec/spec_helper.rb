557fc415b20c1e98ea1c313adec64e82321513c4
Daniel <demartinezgatell@gmail.com>
2019-11-27 17:55:41 +0000

    test infrastructure set up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -98,3 +98,14 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+ENV['RACK_ENV'] = 'test'
+
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+#tell Capybara about our app class
+Capybara.app = Battle
