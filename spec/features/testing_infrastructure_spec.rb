912635dd51858a6f30905642270d641047673f9a
Daniel <demartinezgatell@gmail.com>
2019-11-28 14:03:09 +0000

    unfinsihed entering players

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -4,3 +4,13 @@ feature 'Testing infrastructure' do
     expect(page).to have_content "Hello Battle!"
   end
 end
+
+feature 'Enter names' do
+  scenario 'players can fill in and submit their names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Juan'
+    fill_in :player_2_name, with: 'Samantha'
+    click_button 'Submit'
+    expect(page). to have_content 'Juan vs Samantha'
+  end
+end
